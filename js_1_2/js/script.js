5d23f4b7b27ccf8e39393bc4404bebf8c3e51400
Oleg Lugyna <olugina@ukr.net>
2017-05-10 18:19:32 +0300

    little edits

--- js_1_2/js/script.js
+++ js_1_2/js/script.js
@@ -7,57 +7,63 @@
 
     if (n!=0) {
       var arr = [];
-      for (var i=0; i<n; i++) {
-        arr [i]=x;
+      for (var i = 0; i < n; i++) {
+        arr[i] = x;
       }
 
       var y = arr [0];
-      for (var j=1; j<arr.length; j++) {
-        y = y*arr[j];
+      for (var j = 1; j < arr.length; j++) {
+        y = y * arr[j];
       }
 
       return y;
+
     } else {
-        return 1;
+      return 1;
       }
   }
 
   var x = +prompt ('Введите целое число X - основание степени: ', ' ');
   var n = +prompt ('Введите целое число N - показатель степени: ', ' ');
-    if (n<0) {
+    if (n < 0) {
       alert ('Введите число большее нуля!');
     } else {
-      console.log (exponentiation(x,n).toLocaleString());
+      console.log ( exponentiation(x,n).toLocaleString() );
     }
-
-
 })();
+
+alert ('Вторая задача?');
 
 
 //Задача_2
-/*
+
 (function () {
+  'use strict'
 
   function succesLogin (nameUser) {
     var arr = new Array(5);
-      for (var i=0; i<arr.length; i++) {
-        arr [i]= prompt ('Введите имя: ', ' ');
+
+      for (var i = 0; i < arr.length; i++) {
+        arr[i] = prompt ('Введите имя: ', ' ');
         arr[i] = arr[i].toLowerCase();
       }
 
       var nameUser = prompt ('Введите имя пользователя: ', ' ');
-      nameUser=nameUser.toLowerCase();
 
-    for (var j=0; j<=arr.length; j++) {
-      arr [j]==nameUser;
-        if (arr[j]===nameUser) {
+      nameUser = nameUser.toLowerCase();
+
+    for (var j = 0; j <= arr.length; j++) {
+      arr[j] == nameUser;
+
+        if (arr[j] === nameUser) {
           alert (nameUser[0].toUpperCase() + nameUser.substring(1).toLowerCase() + ', Вы успешно вошли.');
+
          return;
        }
     }
+
     alert ('Вы не верно ввели имя пользователя!');
   }
 
   succesLogin ();
 })();
-*/
