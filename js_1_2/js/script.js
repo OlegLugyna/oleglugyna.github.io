2ba9e5329c3274d6392d4d46bc3fc0fe720212b7
Oleg Lugyna <olugina@ukr.net>
2017-05-14 13:42:12 +0300

    new variant

--- js_1_2/js/script.js
+++ js_1_2/js/script.js
@@ -1,11 +1,65 @@
 // Задача_1
 
+(function ()  {
+  'use strict'
+
+  function exponentiation (base, expon) {
+
+    if (expon!==0) {
+
+      var result = base;
+
+      for (var i = 1; i < expon; i++) {
+        result = result*base;
+      }
+
+      return result;
+
+    } else {
+      return 1;
+    }
+  }
+
+  function getInput (massege) {
+
+    var number = +prompt(massege, '');
+
+    if (window.isNaN(number)) {
+      window.alert('Введите числовое значение!');
+
+      return getInput(massege);
+
+    } else {
+      return number;
+    }
+  }
+
+  function finalCalculation () {
+
+    var x = getInput('Введите цeлое число Х - основание степени', '');
+
+    var n = getInput('Введите целое число N - показатель (экспоненту) степени', '');
+
+      if (n < 0) {
+        return console.log (1/exponentiation (x, n*-1));
+
+      } else {
+        return console.log (exponentiation (x, n));
+      }
+  }
+
+  finalCalculation();
+})();
+
+
+
+/* = Самое первое решение задачи (Эмоциональное)=
 (function () {
   'use strict'
 
   function exponentiation () {
 
-    if (n!=0) {
+    if (n!==0) {
       var arr = [];
       for (var i = 0; i < n; i++) {
         arr[i] = x;
@@ -30,7 +84,7 @@
     } else {
       console.log ( exponentiation(x,n).toLocaleString() );
     }
-})();
+})();*/
 
 confirm ('Вторая задача?');
 
@@ -48,12 +102,12 @@ confirm ('Вторая задача?');
         arr[i] = arr[i].toLowerCase();
       }
 
-      var nameUser = prompt ('Введите имя пользователя: ', ' ');
+      nameUser = prompt ('Введите имя пользователя: ', ' ');
 
       nameUser = nameUser.toLowerCase();
 
     for (var j = 0; j <= arr.length; j++) {
-      arr[j] == nameUser;
+      arr[j] === nameUser;
 
         if (arr[j] === nameUser) {
           alert (nameUser[0].toUpperCase() + nameUser.substring(1).toLowerCase() + ', Вы успешно вошли.');
