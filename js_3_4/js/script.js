55d815a910b6fe2bb7ed65d378767c6151ec15e4
Oleg Lugyna <olugina@ukr.net>
2017-05-19 20:21:45 +0300

    load hw3_4

--- js_3_4/js/script.js
+++ js_3_4/js/script.js
@@ -1,10 +1,9 @@
 (function() {
   'use strict'
 
-  function programmingTest (){
     var test = {
       data: {
-        titele: 'Тест по программированию',
+        title: 'Тест по программированию',
         questions: [
           {
             title: 'Вопрос №1',
@@ -19,60 +18,62 @@
             answers:['Вариант ответа №1', 'Вариант ответа №2', 'Вариант ответа №3']
           }
         ]
+      },
+      createDocument: function() {
+        var  containerTitel = document.createElement('div');
+        var h1 = document.createElement('h1');
+        h1.appendChild(document.createTextNode(this.data.title));
+        containerTitel.appendChild(h1);
+
+        var container = document.createElement('div');
+        container.classList.add('block');
+
+          for (var i = 0, length=this.data.questions.length; i < length; i++) {
+            var header = document.createElement('h2');
+            header.classList.add('question');
+            header.appendChild(document.createTextNode(this.data.questions[i].title));
+            container.appendChild(header);
+
+            var form = document.createElement('form');
+            var ul = document.createElement('ul');
+            ul.classList.add('block__list');
+
+            for (var j = 0, length1=this.data.questions[i].answers.length; j<length1; j++) {
+              var li = document.createElement('li');
+              var label = document.createElement('label');
+              li.appendChild(label);
+
+              var input = document.createElement('input');
+              input. classList.add('answer');
+              input.setAttribute('type', 'checkbox');
+              label.appendChild(input);
+
+              var span = document.createElement('span');
+              span.appendChild(document.createTextNode(test.data.questions[i].answers[j]));
+              label.appendChild(span);
+
+              var br = document.createElement('br');
+              label.appendChild(br);
+
+              ul.appendChild(li);
+            }
+
+            form.appendChild(ul);
+            container.appendChild(form);
+          }
+
+          var button = document.createElement('button');
+          button.innerHTML='Проверить мои результаты';
+          button.className='button';
+          button.setAttribute('type', 'submit');
+          button.setAttribute('formaction', '#');
+
+          var root = document.querySelector('#root');
+          root.appendChild(containerTitel);
+          root.appendChild(container);
+          root.appendChild(button);
       }
     };
 
-    function creatTitele () {
-      var root = document.getElementById('root');
-      var h1 = document.createElement('h1');
-      h1.appendChild(document.createTextNode(test.data.titele));
-      root.appendChild(h1);
-  }
-
-    function creatQustionaire() {
-
-      for (var key in test.data.questions) {
-        var questionsTitle = document.createElement('p');
-        questionsTitle.className='question';
-        questionsTitle.appendChild(document.createTextNode(test.data.questions[key].title));
-        root.appendChild(questionsTitle);
-
-          for (var i = 0, length=test.data.questions[key].answers.length; i < length; i++) {
-            var label = document.createElement('label');
-            root.appendChild(label);
-
-            var elInput = document.createElement('input');
-            questionsTitle.className='answer';
-            elInput.setAttribute('type', 'checkbox');
-            label.appendChild(elInput);
-
-            var span = document.createElement('span');
-            span.appendChild(document.createTextNode(test.data.questions[key].answers[i]));
-            label.appendChild(span);
-
-            var br = document.createElement('br');
-            label.appendChild(br);
-          }
-      }
-    }
-
-    function creatButton () {
-      var root = document.getElementById('root');
-      var button = document.createElement('button');
-      button.innerHTML="Проверить мои результаты";
-      button.className='button';
-      button.setAttribute('type', 'submit');
-      button.setAttribute('formaction', '#')
-      root.appendChild(button);
-    }
-
-    creatTitele();
-    creatQustionaire();
-    creatButton();
-
-  }
-
-  programmingTest ();
-  //console.log (root);
-
+test.createDocument();
 })();
