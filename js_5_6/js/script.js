f1771d3cea453c154f22c8982dd23ffe56b55d37
Oleg Lugyna <olugina@ukr.net>
2017-05-24 01:24:50 +0300

    last variant

--- js_5_6/js/script.js
+++ js_5_6/js/script.js
@@ -2,14 +2,17 @@
   'use strict'
 
   var start = Date.now();
+  var timerIndicator = 0;
 
-
+  var colon = document.querySelectorAll('.colon');
+  colon[0].innerHTML=':';
+  colon[1].innerHTML=':';
 
   function updateTime() {
       var root = document.getElementById('root');
       var milliSeconds = document.querySelector('.milli_seconds');
       var timeOut = Date.now();
-      var timerIndicator = (timeOut - start);
+      timerIndicator = (timeOut - start);
       //root.className='time_elem';
       var timeMilliSeconds = new Date(timerIndicator).getMilliseconds();
       milliSeconds.innerHTML=timeMilliSeconds;
@@ -24,7 +27,8 @@
         }
 
 
-      var minutes = root.querySelector('.minutes');
+
+      var minutes = document.querySelector('.minutes');
       var timeMin = new Date (timerIndicator).getMinutes();
 
         if (timeMin<10) {
@@ -32,7 +36,6 @@
         } else {
           minutes.innerHTML = timeMin;
         }
-
 
       var seconds = document.querySelector('.seconds');
       var timeSec = new Date(timerIndicator).getSeconds();
@@ -44,35 +47,34 @@
         }
   }
 
+  var end;
+  function startTimer() {
+  end = setInterval(updateTime, 5);
+  }
 
-    var end = setInterval(updateTime, 5);
-    //clearInterval(end);
+  function stopTimer() {
+    clearInterval(end);
+  }
 
-
-  //function buttonStart () {
   var buttonStart = document.querySelector('.button__start');
-     var button1 = document.createElement('button');
-     button1.innerHTML='START';
-     button1.className='button';
-     button1.setAttribute('type', 'submit');
-     button1.setAttribute('formaction', '#');
-     buttonStart.appendChild(button1);
+  var button1 = document.createElement('button');
+  button1.innerHTML='START';
+  button1.className='button';
+  button1.setAttribute('id', 'button__start');
+  button1.setAttribute('type', 'submit');
+  button1.setAttribute('formaction', '#');
+  buttonStart.appendChild(button1);
+  buttonStart.addEventListener('click', startTimer);
 
-     var buttonClear = document.querySelector('.button__clear');
-     var button = document.createElement('button');
-     button.innerHTML='CLEAR';
-     button.className='button';
-     button.setAttribute('type', 'submit');
-     button.setAttribute('formaction', '#');
-     buttonClear.appendChild(button);
-
-
-   //}
-
-   //buttonStart();
-
-
-
+  var buttonClear = document.querySelector('.button__clear');
+  var button = document.createElement('button');
+  button.innerHTML='CLEAR';
+  button.className='button';
+  button.setAttribute('id', 'button__clear');
+  button.setAttribute('type', 'submit');
+  button.setAttribute('formaction', '#');
+  buttonClear.appendChild(button);
+  buttonClear.addEventListener('click', stopTimer);
 
 })();
 
