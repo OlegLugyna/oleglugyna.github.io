519e3c5e3352f87d33a67c9521a8021e1345cbee
Oleg Lugyna <olugina@ukr.net>
2017-06-02 00:24:51 +0300

    load homework7-8

--- js_7_8/forms/js/script.js
+++ js_7_8/forms/js/script.js
@@ -1,18 +1,19 @@
 'use strict'
 
   $(function() {
-    var $linkFirst = $('input#first_name');
-    $linkFirst.on('click', function(event) {
+    var $linkFirst = $('input.input-field');
+    $linkFirst.click(function(event) {
       var $title = $(this).attr('title');
-      var $tooltips = $('#tooltips');
+      var $tooltips = $('.tooltip');
       $tooltips.text($title)
                 .css ({
+                  'display':'block',
                   'top': event.pageY + 5,
                   'left': event.pageX + 5
                 })
                 .show();
     }).mouseout (function () {
-      var $tooltips = $('#tooltips');
+      var $tooltips = $('.tooltip');
       $tooltips.hide()
               .text('')
               .css({
@@ -22,79 +23,28 @@
     });
 });
 
-  $(function() {
-    var $linkSecond = $('input#last_name');
-    $linkSecond.on('click', function(event) {
-      var $title = $(this).attr('title');
-      var $tooltips = $('#tooltips');
-      $tooltips.text($title)
+$(function() {
+  var $linkButton = $('button');
+  $linkButton.on ('click', function() {
+    var $title = $('.input-field').attr('title');
+    var $tooltips = $('span.tooltip');
+    $tooltips.each(function(event) {
+      $(this).text($title)
                 .css ({
-                  'top': event.pageY + 5,
+                  'display':'block',
+                  'top': event.pageY - 5,
                   'left': event.pageX + 5
-                })
-                .show();
-    }).mouseout (function () {
-      var $tooltips = $('#tooltips');
-      $tooltips.hide()
-              .text('')
-              .css({
-                'top': 0,
-                'css': 0
-              });
-    });
-  });
+                });
+    })
+            .show();
+      }).mouseout (function () {
+        var $tooltips = $('.tooltip');
+        $tooltips.hide()
+                .text('')
+                .css({
+                  'top': 0,
+                  'css': 0
+                });
 
-  $(function() {
-    var $linkAddress = $('input#address');
-    $linkAddress.on('click', function(event) {
-      var $title = $(this).attr('title');
-      var $tooltips = $('#tooltips');
-      $tooltips.text($title)
-                .css ({
-                  'top': event.pageY + 5,
-                  'left': event.pageX + 5
-                })
-                .show();
-    }).mouseout (function () {
-      var $tooltips = $('#tooltips');
-      $tooltips.hide()
-              .text('')
-              .css({
-                'top': 0,
-                'css': 0
-              });
-    });
-  });
-
-  $(function() {
-    var $linkButton = $('button');
-    $linkButton.on ('click', function(event) {
-      var $title = $('input#first_name').attr('title');
-      var $tooltips = $('#tooltips');
-      $tooltips.text($title)
-                .css ({
-                  'top': event.pageY - 205,
-                  'left': event.pageX + 350
-                })
-                .show();
-
-      var $titleLast = $('input#last_name').attr('title');
-            var $tooltipsLast = $('#tooltips');
-            $tooltipsLast.text($titleLast)
-                      .css ({
-                        'top': event.pageY - 150,
-                        'left': event.pageX + 350
-                      })
-                .show();
-    });
-    // }).mouseout (function () {
-    //   var $tooltips = $('#tooltips');
-    //   $tooltips.hide()
-    //           .text('')
-    //           .css({
-    //             'top': 0,
-    //             'css': 0
-    //           });
-
-
+          });
   });
