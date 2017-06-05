b78f021b2542acf2f59a329a352894f557cd5390
Oleg Lugyna <olugina@ukr.net>
2017-06-05 14:48:57 +0300

    some edits

--- js_7_8/forms1/js/script.js
+++ js_7_8/forms1/js/script.js
@@ -4,45 +4,50 @@
     var $linkInput = $('input.input-field');
     $linkInput.click(function(event) {
       var $title = $(this).attr('title');
-      var $wrapperForm = $('.wrapper-form');
-      $wrapperForm.css({'position':'static'});
-      var $tooltip = $('.tooltip');
-      $tooltip.text($title)
+      // var $wrapperForm = $('.wrapper-form');
+      // $wrapperForm.css({'position':'static'});
+      var $tooltips = $('#tooltips-div');
+      $tooltips.text($title)
                 .css ({
+
                   'top': event.pageY + 5,
                   'left': event.pageX + 10,
                 })
                 .show();
-    })
+    });
     $linkInput.mouseout (function () {
-      var $tooltips = $('.tooltip');
+      var $tooltips = $('#tooltips-div');
       $tooltips.hide()
+              // .text('')
               .css({
-                'display':'none',
+                // 'display':'none',
                 'top': 0,
                 'left': 0
               });
-            var $wrapperForm = $('.wrapper-form');
-            $wrapperForm.css({'position':'relative'});
+
+    // var $wrapperForm = $('.wrapper-form');
+    // $wrapperForm.css({'position':'relative'});
     });
 
     var $linkButton = $('button');
     $linkButton.on('click',showAllTooltips);
+    var $title = parent().find('.input-field').attr('title');
 
     function showAllTooltips() {
+
       var $tooltip = $('.tooltip');
-      $tooltip.addClass('visible').css ({
+      $tooltip.text($title).addClass('visible').
+      css ({
           'display':'block',
           'top': '50%',
-          'left': '73%'
+          'left': '73%',
       });
       setTimeout(hideAllTooltips, 2000);
     }
 
     function hideAllTooltips() {
-      $('.tooltip').removeClass('_visible').css ({
-        "display":'none'
-      });
+      $('.tooltip').removeClass('visible')
+                    .css ({'display':'none'});
     }
 
 });
